9abe01c7f02b40893a209e83efa8c2e36780c76c
Nelson Gougou <nelsongougou@MacBook-Pro-de-Nelson.local>
2024-12-25 13:25:00 +0000

    docs(conf): add extension to handle google docstring style

--- docs_source/source/conf.py
+++ docs_source/source/conf.py
@@ -20,7 +20,11 @@ release = '0.5.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.githubpages",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
