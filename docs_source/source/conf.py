2549cd7a99e8b2cbb06b242119943b6d5fe80a29
Nelson Gougou <nelsongougou@MacBook-Pro-de-Nelson.local>
2024-12-25 15:13:21 +0000

    docs(conf): update pythonpath

--- docs_source/source/conf.py
+++ docs_source/source/conf.py
@@ -7,7 +7,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../../starburst_api"))
+sys.path.insert(0, os.path.abspath("../../datamesh_checker"))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
